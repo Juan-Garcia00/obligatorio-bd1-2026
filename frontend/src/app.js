e63921536948429d5a494e38268b9b4dcffc2579
Garcaaaa <juandigar2006@gmail.com>
2026-06-11 12:40:35 -0300

    Acomodo el app para probar front

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -15,14 +15,38 @@ function cargarActividades() {
       data.forEach((actividad) => {
         const div = document.createElement("div");
         div.className = "tarjeta";
-        div.innerHTML = `
-                    <h3>${actividad.nombre}</h3>
-                    <p><strong>Disciplina:</strong> ${actividad.disciplina}</p>
-                    <p><strong>Lugar:</strong> ${actividad.espacio}</p>
-                    <p><strong>Horario:</strong> ${actividad.dia} a las ${actividad.horario}</p>
-                    <p><strong>Cupo Máximo:</strong> ${actividad.cupo}</p>
-                    <p class="estado">Estado: ${actividad.estado}</p>
-                `;
+        div.innerHTML =`
+            <h3>${actividad.nombre}</h3>
+
+            <p>
+            <strong>Disciplina:</strong>
+            ${actividad.disciplina_nombre}
+            </p>
+
+            <p>
+            <strong>Espacio:</strong>
+            ${actividad.espacio_nombre}
+            </p>
+
+            <p>
+            <strong>Día:</strong>
+            ${actividad.dia_semana}
+            </p>
+
+            <p>
+            <strong>Horario:</strong>
+            ${actividad.hora_inicio} - ${actividad.hora_fin}
+            </p>
+
+            <p>
+            <strong>Cupo:</strong>
+            ${actividad.cupo_maximo}
+            </p>
+
+            <p class="estado">
+            Estado: ${actividad.estado}
+            </p>
+          `;
         contenedor.appendChild(div);
       });
     })
